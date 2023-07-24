bff7385c74cbf3aebc67bcc9d10ec939405a9de3
FLOYD <sungmin.park5941@gmail.com>
2023-07-24 15:56:54 +0900

    #2-10 BFS를 이용한 길찾기 구현

--- Player.cs
+++ Player.cs
@@ -38,9 +38,67 @@ namespace Algorithm
             
             _board = board;
 
+            BFS();
+
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while(q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for(int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+                    //예외 처리
+                    if(nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall) 
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+            //부모와 자신의 좌표가 같은지 확인
+            while (parent[y,x].Y != y || parent[y,x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
+            //도착지점 부터 역순으로 시작지점까지 비교하면서 가기 때문에 Reverse를 사용해서 뒤집는다
+        }
+        //우수법
+        void RightHand()
+        {
             //현재 바라보고 있는 방향을 기준으로 죄표 변화를 나타낸다
             int[] frontY = new int[] { -1, 0, 1, 0 };
-            int[] frontX = new int[] { 0, -1, 0, 1};
+            int[] frontX = new int[] { 0, -1, 0, 1 };
             int[] rightY = new int[] { 0, -1, 0, 1 };
             int[] rightX = new int[] { 1, 0, -1, 0 };
 
@@ -48,7 +106,7 @@ namespace Algorithm
 
             //모든 경로를 미리 계산
             //목적지에 도착하기 전에는 계속 실행
-            while (PosY != board.DestY || PosX != board.DestX)
+            while (PosY != _board.DestY || PosX != _board.DestX)
             {
                 //1. 현재 바라보는 기준으로 오른쪽으로 갈 수 있는지 확인
                 if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
@@ -75,7 +133,6 @@ namespace Algorithm
                     _dir = (_dir + 1 + 4) % 4;
                 }
             }
-
         }
 
         const int MOVE_TICK = 10;
